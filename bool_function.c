766ab0d306bd096ad961bd72ec4818e26d21ba77
Hamed <hamed.2012.darzi@gmail.com>
2024-08-24 13:16:01 +0200

    add function

--- bool_function.c
+++ bool_function.c
@@ -3,9 +3,9 @@
 
 bool ifis_prime(int n){
     bool esiste_prime = (n <2);
-    for (int i = 0; i < n; ++i) {
-        
-
+    for (int i = 0; i < n && !esiste_prime; ++i) {
+        if(n % i == 0)
+            esiste_prime = true;
     }
 }
 
