aaa7a936cf5a9164e9ff2043969c5f5dcb3cc6ed
Hamed <hamed.2012.darzi@gmail.com>
2024-08-24 13:20:49 +0200

    final

--- bool_function.c
+++ bool_function.c
@@ -3,12 +3,19 @@
 
 bool ifis_prime(int n){
     bool esiste_prime = (n <2);
-    for (int i = 0; i < n && !esiste_prime; ++i) {
+    for (int i = 2; i < n && !esiste_prime; ++i) {
         if(n % i == 0)
             esiste_prime = true;
     }
+    return !esiste_prime;
 }
 
 int main(void){
+    for (int i = 0; i < 100; ++i) {
+        if(ifis_prime(i))
+            printf("%d ", i);
+
+    }
+    puts("");
 
 }
