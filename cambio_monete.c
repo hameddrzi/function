e5fe4c921de7a585dd0f3b563babf659e9bfbce7
Hamed <hamed.2012.darzi@gmail.com>
2024-08-24 15:00:20 +0200

    Description

--- cambio_monete.c
+++ cambio_monete.c
@@ -2,14 +2,14 @@
 #include <stdbool.h>
 
 
-int cambio(int prezzo, int monete){
-    int cambio = prezzo / monete;
-    int resto = prezzo % monete;
+int cambio(int cent, int monete){
+    int cambio = cent / monete;     // 50cent / last block of array[50] = 1
+    int resto = cent % monete;      // 90cent % last block of array[50] = rest 40
 
     if(cambio > 0){
         printf("%d da %d cent\n", cambio, monete);
     }
-    return resto;
+    return resto;                   //return 40 in the main in the loop
 }
 
 
@@ -17,11 +17,11 @@ int main(){
     int cent;
     printf("insert moente: ");
     scanf("%d", &cent);
-    #define NUM_CENT 6
+#define NUM_CENT 6
     const int monete[NUM_CENT] = {1, 2, 5, 10, 20, 50};
 
     for (size_t i = NUM_CENT ; i >0 ; --i) {
-        cent = cambio(cent, monete[i-1]);
+        cent = cambio(cent, monete[i]);
 
     }
 }
