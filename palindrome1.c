73c35c9b002bda08094687b191de1f7054d4c4dd
Hamed <hamed.2012.darzi@gmail.com>
2024-08-29 23:17:58 +0200

    test_if word?

--- palindrome1.c
+++ palindrome1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <string.h>
+#define MAX 50
 int main(){
-    dsdsds
+    char a[MAX+1];
+    char b[MAX+1];
+    b[MAX+1] = a[MAX+1];
+
+    bool palindrome = true;
+
+    scanf("%50s", a);
+    for (size_t i = 0; i = MAX; ++i) {
+        for (size_t j = 0; j = MAX; --j) {
+            if(!(a[i])== b[j-1])
+                palindrome = false;
+        }
+
+    }
+    printf("palindrome", palindrome);
 
 }
