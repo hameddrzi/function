ea4c9ace8fb1fec5166f51d2b2baacb5e02a478d
Hamed <hamed.2012.darzi@gmail.com>
2024-08-30 17:51:28 +0200

    add another function
     the program must copy the string by reversing the order of the characters and print the original string and the reversed string.

--- palindroma_function.c
+++ palindroma_function.c
@@ -12,11 +12,24 @@ bool pali(const char *str){
     return palindroma;
 }
 
+void invertt(char *word, char *invert){
+    size_t len = strlen(word);
+    for (int i = 0; i < len; ++i) {
+        invert[i]= word[len - i - 1];
+
+    }
+    invert[len] = '\0';
+}
+
 #define MAX 50
 int main() {
-    char word[MAX+1];
+    char word[MAX+1], invert[MAX+1];
     scanf("%50s", word);
 
-    if(pali(&word))printf("palindroma");
-    else printf("NOT palindroma");
+    if(pali(&word)){printf("palindroma");}
+    else{
+        invertt(&word, &invert);
+        printf("%s", invert);
+
+    }
 }
