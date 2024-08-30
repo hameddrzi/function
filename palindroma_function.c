6ed6e99e1152fc1ff794a16c18eac6f875a5d508
Hamed <hamed.2012.darzi@gmail.com>
2024-08-30 16:19:18 +0200

    add with function

--- palindroma_function.c
+++ palindroma_function.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
+bool pali(const char *str){
+    size_t len = strlen(str);
+    bool palindroma = true;
+    for (size_t i = 0; i < len/2 && palindroma; ++i) {
+        if(str[i] != str[len - i  - 1]  )
+            palindroma = false;
+    }
+    return palindroma;
+}
+
+#define MAX 50
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    char word[MAX+1];
+    scanf("%50s", word);
+
+    if(pali(&word))printf("palindroma");
+    else printf("NOT palindroma");
 }
